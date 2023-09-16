890d02295a1c0cb1b39e9c5bace581488b054363
kaush <kaush@10.0.0.211>
2023-09-16 18:21:33 -0400

    First Commit Lab1

---
+++ src/Model/product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author kaush
+ */
+public class product {
+    
+    private String name;
+    private String descr;
+    private String availnum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getAvailnum() {
+        return availnum;
+    }
+
+    public void setAvailnum(String availnum) {
+        this.availnum = availnum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+}
